f0069923d1695daab58d56904b8b1faa80f47778
huangzhenmu <946624404@qq.com>
2019-11-24 22:44:23 +0800

    添加lombok

--- tenquare_common/src/main/java/com/hzm/entity/Result.java
+++ tenquare_common/src/main/java/com/hzm/entity/Result.java
@@ -1,5 +1,12 @@
 package com.hzm.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Result {
     private boolean flag;
     private Integer code;
